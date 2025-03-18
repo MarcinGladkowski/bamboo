5edb7fa4b8847e63caf4f0e90a21a0911590dddc
MarcinGladkowski <marcingladkowski@gmail.com>
2025-03-18 08:55:28 +0100

    cocoa nimbs: load data and coert ... into 0

--- cacao_nibs/main.py
+++ cacao_nibs/main.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 
@@ -7,4 +8,13 @@ df = pd.read_excel(
     header=4, # columns names from row 4
 )
 
-print(df.columns)
+df = df.drop([0]) # remove column contains measures
+
+df = df.rename(columns={'Unnamed: 0': 'Date'})
+df = df.set_index('Date')
+df = df.infer_objects(copy=False)
+df = df.apply(lambda x: x.replace('...', 0))
+df = df.apply(pd.to_numeric, errors='coerce') # replace ... with Nan
+df = df.fillna(0)
+
+
