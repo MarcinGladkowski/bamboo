9942e8284e7eb619c01f238cb8b1ebc88c245f0f
MarcinGladkowski <marcingladkowski@gmail.com>
2025-03-18 09:58:40 +0100

    cocoa nimbs: plot data using seaborn by task description

--- cacao_nibs/main.py
+++ cacao_nibs/main.py
@@ -1,29 +1,35 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
-
+import seaborn as sns
 
 
 df = pd.read_excel(
     io='./data/CMO-Historical-Data-Monthly.xlsx',
     sheet_name='Monthly Prices',
     header=4, # columns names from row 4
+    na_values=['…', '...'],
+    parse_dates=[0],
+    date_format='%YM%m'
 )
 
 df = df.drop([0]) # remove column contains measures
-
 df = df.rename(columns={'Unnamed: 0': 'Date'})
-
-df['Date'] = pd.to_datetime(df['Date'], format='%YM%m').dt.strftime('%Y-%m')
 df = df.set_index('Date')
-
-df = df.apply(pd.to_numeric, errors='coerce') # replace ... with Nan
-df = df.fillna(0)
-
 to_plot = df['Cocoa']
 
-ax = to_plot.plot(kind='line')
-plt.tight_layout()
+# ax = to_plot.plot(kind='line')
+# plt.tight_layout()
+# plt.show()
+
+(
+    df
+    .iloc[-60:]
+    ['Cocoa']
+    .pipe(lambda df_: sns.lineplot(df_, marker='o'))
+    .set(xlabel='Date',
+         ylabel='Price',
+         title='Cocoa Prices')
+)
 
 plt.show()
 
