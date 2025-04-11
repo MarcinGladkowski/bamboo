c6a527a9eeed0e2462bf89c0f4bea3bc01d58cca
MarcinGladkowski <marcingladkowski@gmail.com>
2025-04-11 15:23:21 +0200

    us airport traffic: plot data - working solution

--- us_airport_traffic/main.py
+++ us_airport_traffic/main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from matplotlib import pyplot as plt
 
 atlanta = pd.read_csv(
     filepath_or_buffer="data/atlanta_terminal_e_A171_2023-02-04-2025-08-04.csv",
@@ -20,20 +21,33 @@ san_francisco = format_flight_date_time(san_francisco)
 flights = pd.concat([atlanta, san_francisco])
 flights.set_index(['AirportCode', 'FlightDateTime'], inplace=True)
 
+# Ensure FlightDateTime is a DatetimeIndex
+flights.index = flights.index.set_levels(
+    [flights.index.levels[0], pd.to_datetime(flights.index.levels[1])]
+)
+
 # Group by name and monthly datetime
 monthly_grouped = flights.groupby(
     [pd.Grouper(level='AirportCode'), pd.Grouper(level='FlightDateTime', freq='M')]
 ).sum()
 
-# Reset index for plotting
-monthly_grouped.reset_index(inplace=True)
+# Reset index to prepare data for plotting
+monthly_grouped = monthly_grouped.reset_index()
 
 # Plot the data
-for name, group in monthly_grouped.groupby('name'):
-    plt.plot(group['FlightDateTime'], group['NonUsaPassengerCount'], label=name)
+plt.figure(figsize=(10, 6))
+for airport_code in monthly_grouped['AirportCode'].unique():
+    airport_data = monthly_grouped[monthly_grouped['AirportCode'] == airport_code]
+    plt.plot(
+        airport_data['FlightDateTime'],
+        airport_data['NonUsaPassengerCount'],  # Replace with the column you want to plot
+        label=airport_code
+    )
 
+plt.title('Monthly Traffic by Airport')
 plt.xlabel('Date')
-plt.ylabel('Value')
-plt.title('Monthly Data by Name')
-plt.legend()
+plt.ylabel('Traffic')
+plt.legend(title='Airport Code')
+plt.grid(True)
+plt.tight_layout()
 plt.show()
